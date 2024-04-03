4b7a979630e321fc26ee890538e2e255dfe3bbdd
clara-almeida <144968459+clara-almeida@users.noreply.github.com>
2024-04-02 21:19:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-  return "Olá, <b>tudo bem</b>?"
-
-@app.route("/teste")
-def teste():
-  return "Essa página é um <b>teste</b>" 
-
-@app.route("/telegram", methods=["POST"])
-def telegram_update():
-    update = request.json
-    url_envio_mensagem = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    chat_id = update ["message"]["chat"]["id"]
-    mensagem = {"chat_id": chat_id, "text": "mensagem <b> recebida</b>!", "parse_mode": "HTML"}
-    requests.post(url_envio_mensagem, data=mensagem)
-    return "ok"
+def apresentacao():
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Melissa Duarte</title>
+    </head>
+    <body>
+        <h1>Melissa Duarte</h1>
+        <h3>Jornalista, 26 anos, Brasília</h3>
+        <img src="https://images.jota.info/wp-content/uploads/2023/05/9v9c4401-escolhida-1-682x1024.jpg" 
+        alt="Melissa Duarte" width="250" height="350">
+        <h3>Sobre mim:</h3>
+        <p>Jornalista especializada nas <b>coberturas de saúde e de políticas públicas</b>. Atualmente, é <b>repórter do JOTA</b>.</p> 
+        <p>Trabalhou nos jornais <b>O Globo, Poder360 e Correio Braziliense e também atuou nas áreas de cultura, gastronomia, política e educação.</b></p> 
+        <p><b>Cursa o Master em Jornalismo de Dados, Automação e Data Storytelling</b> no Insper e <b>se formou em Jornalismo na Universidade de Brasília (UnB)</b>, período em que ganhou o Prêmio Nacional Expocom 2019 na categoria produção de TV.</p>
+    </body>
+</html>
