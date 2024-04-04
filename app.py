b9e36ccf5385162f048d21c267cb76d92d2b68ca
“clara-almeida” <clara.almeidagom@gmail.com>
2024-04-04 18:15:24 -0300

    teste 4

--- app.py
+++ app.py
@@ -15,3 +15,19 @@ def contato():
 @app.route("/materias")
 def materias():
   return render_template('materias.html') 
+
+@app.route("/discursos")
+def discursos():
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+         <title> Highlights Discursos do Presidente</title>
+    </head>
+    <body>
+      <h1> Confira os principais trechos dos últimos discursos presidenciais</h1>
+      <p> 
+        Veja, abaixo, as últimas declarações:
+        <ul>
+    """
+    
